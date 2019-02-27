4ba0280c554832f47919513a01654263b0fdc181
Carlos Federico <carlos.federico005@gmail.com>
2019-02-27 15:55:21 -0300

    Added HW 2, performance differences using build-in functions and vectorization.

---
+++ homework_2/preallocation.m
@@ -0,0 +1,26 @@
+# Unallocated
+tic
+
+for i = 1:1024
+  for j = 1:1024
+    unallocatedMatrix(i, j) = i + 2*j;
+  endfor
+endfor
+
+time1 = toc;
+
+printf('Unallocated: %d\n', time1); # ~5.17s
+
+# Preallocated
+tic
+
+preallocatedMatrix = zeros(1024, 1024);
+for i = 1:1024
+  for j = 1:1024
+    preallocatedMatrix(i, j) = i + 2*j;
+  endfor
+endfor
+
+time2 = toc;
+
+printf('Preallocated: %d\n', time2); # ~5.02s
